fadfb5bc970cb3a7b81273ce00c5df27b707b9df
priyanshu <priyanshu0dubey@gmail.com>
2021-09-22 09:45:01 +0530

    firebase deatils

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyAqHq7B_YQlwgSp549DDHxqIO_xBd4K2NM",
+    authDomain: "todolist-2b078.firebaseapp.com",
+    projectId: "todolist-2b078",
+    storageBucket: "todolist-2b078.appspot.com",
+    messagingSenderId: "467644493273",
+    appId: "1:467644493273:web:4e36eeb93ad117c7b23dc8"
+  }
 };
